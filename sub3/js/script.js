b87aec29b7703984d86d8fe5d5137ee82957127e
pej556 <dmswn6173@naver.com>
2025-07-31 21:43:14 +0900

    Add files via upload

---
+++ sub3/js/script.js
@@ -0,0 +1,10 @@
+//모바일 전용 메뉴바
+document.querySelector('.open_btn').addEventListener('click', () => {
+  document.querySelector('.menu_box').classList.add('active');
+  document.querySelector('.menu_black').classList.add('active');
+});
+
+document.querySelector('.close_btn').addEventListener('click', () => {
+  document.querySelector('.menu_box').classList.remove('active');
+  document.querySelector('.menu_black').classList.remove('active');
+});
